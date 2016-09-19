eb236e546326cf68883647b86a4b2abc76f3eb13
Joao Correia <joaope@gmail.com>
2016-09-19 03:23:30 +0100

    implement assembly info rewrite

--- src/LocalAppVeyor.Engine/Internal/Steps/AssemblyInfoRewriteStep.cs
+++ src/LocalAppVeyor.Engine/Internal/Steps/AssemblyInfoRewriteStep.cs
@@ -1,10 +1,17 @@
-﻿using System;
+using System;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace LocalAppVeyor.Engine.Internal.Steps
 {
     internal class AssemblyInfoRewriteStep : IInternalEngineStep
     {
+        private static readonly Regex AssemblyVersionPattern = new Regex(@"AssemblyVersion\("".+""\)", RegexOptions.Compiled);
+
+        private static readonly Regex AssemblyFileVersionPattern = new Regex(@"AssemblyFileVersion\("".+""\)", RegexOptions.Compiled);
+
+        private static readonly Regex AssemblyInformationalVersionPattern = new Regex(@"AssemblyInformationalVersion\("".+""\)", RegexOptions.Compiled);
+
         public bool Execute(ExecutionContext executionContext)
         {
             if (!executionContext.BuildConfiguration.AssemblyInfo.Patch)
@@ -16,6 +23,14 @@ namespace LocalAppVeyor.Engine.Internal.Steps
             {
                 executionContext.Outputter.WriteWarning("No assembly info files specified.");
                 return true;
+            }
+
+            if (string.IsNullOrEmpty(executionContext.BuildConfiguration.AssemblyInfo.AssemblyFileVersion) &&
+                string.IsNullOrEmpty(executionContext.BuildConfiguration.AssemblyInfo.AssemblyVersion) &&
+                string.IsNullOrEmpty(executionContext.BuildConfiguration.AssemblyInfo.AssemblyInformationalVersion))
+            {
+                executionContext.Outputter.WriteWarning("No versioning information provided to re-write AssemlyInfo files with.");
+                return false;
             }
 
             foreach (var assemblyInfoFile in Directory.EnumerateFiles(
@@ -32,12 +47,36 @@ namespace LocalAppVeyor.Engine.Internal.Steps
             return true;
         }
 
-        private bool RewriteAssemblyInfoFile(ExecutionContext executionContext, string filePath)
+        private static bool RewriteAssemblyInfoFile(ExecutionContext executionContext, string filePath)
         {
+            executionContext.Outputter.Write($"Re-writing '{filePath}'...");
+
             try
             {
+                var fileContent = File.ReadAllText(filePath);
 
+                if (!string.IsNullOrEmpty(executionContext.BuildConfiguration.AssemblyInfo.AssemblyVersion))
+                {
+                    fileContent = AssemblyVersionPattern.Replace(
+                        fileContent,
+                        $@"AssemblyVersion(""{executionContext.BuildConfiguration.AssemblyInfo.AssemblyVersion}"")");
+                }
 
+                if (!string.IsNullOrEmpty(executionContext.BuildConfiguration.AssemblyInfo.AssemblyFileVersion))
+                {
+                    fileContent = AssemblyFileVersionPattern.Replace(
+                        fileContent,
+                        $@"AssemblyFileVersion(""{executionContext.BuildConfiguration.AssemblyInfo.AssemblyFileVersion}"")");
+                }
+
+                if (!string.IsNullOrEmpty(executionContext.BuildConfiguration.AssemblyInfo.AssemblyInformationalVersion))
+                {
+                    fileContent = AssemblyInformationalVersionPattern.Replace(
+                        fileContent,
+                        $@"AssemblyInformationalVersion(""{executionContext.BuildConfiguration.AssemblyInfo.AssemblyInformationalVersion}"")");
+                }
+
+                File.WriteAllText(filePath, fileContent);
                 return true;
             }
             catch (Exception e)
