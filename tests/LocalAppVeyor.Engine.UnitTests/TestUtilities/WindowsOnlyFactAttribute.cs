88c5b1011c5c67f73cae982ce58515347f339cf9
Joao Correia <joaope@gmail.com>
2019-08-07 00:03:04 +0100

    OS specific tests

---
+++ tests/LocalAppVeyor.Engine.UnitTests/TestUtilities/WindowsOnlyFactAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.InteropServices;
+using Xunit;
+
+namespace LocalAppVeyor.Engine.UnitTests.TestUtilities
+{
+    internal sealed class WindowsOnlyFactAttribute : FactAttribute
+    {
+        public WindowsOnlyFactAttribute()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Skip = "Windows-only test";
+            }
+        }
+    }
+}
