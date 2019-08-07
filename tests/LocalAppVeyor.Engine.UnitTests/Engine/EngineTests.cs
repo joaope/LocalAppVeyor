cb0ad342ae63086b49dee72ab6eab8b7423b34d9
Joao Correia <joaope@gmail.com>
2019-08-07 23:09:03 +0100

    engine tests need to use real FS because of Process

--- tests/LocalAppVeyor.Engine.UnitTests/Engine/EngineTests.cs
+++ tests/LocalAppVeyor.Engine.UnitTests/Engine/EngineTests.cs
@@ -1,5 +1,4 @@
 ﻿using System.IO.Abstractions;
-using System.IO.Abstractions.TestingHelpers;
 using Moq;
 using Xunit.Abstractions;
 
@@ -10,10 +9,20 @@ namespace LocalAppVeyor.Engine.UnitTests.Engine
         private readonly ITestOutputHelper _outputHelper;
         private readonly Mock<IPipelineOutputter> _outputterMock = new Mock<IPipelineOutputter>();
         private readonly EngineConfiguration _engineConfiguration;
-        private readonly IFileSystem _fileSystem = new MockFileSystem();
+        private readonly IFileSystem _fileSystem = new FileSystem();
 
         public EngineTests(ITestOutputHelper outputHelper)
         {
+            var currentDirectory =
+                _fileSystem.Path.Combine(_fileSystem.Path.GetTempPath(), _fileSystem.Path.GetRandomFileName());
+
+            if (!_fileSystem.Directory.Exists(currentDirectory))
+            {
+                _fileSystem.Directory.CreateDirectory(currentDirectory);
+            }
+
+            _fileSystem.Directory.SetCurrentDirectory(currentDirectory);
+
             _outputHelper = outputHelper;
             _engineConfiguration = new EngineConfiguration(_fileSystem.Directory.GetCurrentDirectory(), _outputterMock.Object, _fileSystem);
         }
