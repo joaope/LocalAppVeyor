eefe5400602759d6a6532745fd277c762a0db090
Joao Correia <joaope@gmail.com>
2019-08-08 00:01:14 +0100

    rename unix tests

---
+++ tests/LocalAppVeyor.Engine.UnitTests/TestUtilities/UnixOnlyFactAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System.IO.Abstractions.TestingHelpers;
+using Xunit;
+
+namespace LocalAppVeyor.Engine.UnitTests.TestUtilities
+{
+    internal sealed class UnixOnlyFactAttribute : FactAttribute
+    {
+        public UnixOnlyFactAttribute()
+        {
+            if (!MockUnixSupport.IsUnixPlatform())
+            {
+                Skip = "Unix-only test";
+            }
+        }
+    }
+}
